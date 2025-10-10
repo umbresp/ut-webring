c2caa0ea0dcefb9846ff76c47d67a5ed8350129c
Sasha Huang <azhuang@utexas.edu>
2025-10-10 14:07:32 -0500

    Add small listing

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import cloudflareLogo from './assets/Cloudflare_Logo.svg'
-import './App.css'
+import { webring } from '../sites.json'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [name, setName] = useState('unknown')
 
   return (
     <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-        <a href='https://workers.cloudflare.com/' target='_blank'>
-          <img src={cloudflareLogo} className='logo cloudflare' alt='Cloudflare logo' />
-        </a>
-      </div>
-      <h1>Vite + React + Cloudflare</h1>
-      <div className='card'>
-        <button
-          onClick={() => setCount((count) => count + 1)}
-          aria-label='increment'
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <div className='card'>
-        <button
-          onClick={() => {
-            fetch('/api/')
-              .then((res) => res.json() as Promise<{ name: string }>)
-              .then((data) => setName(data.name))
-          }}
-          aria-label='get name'
-        >
-          Name from API is: {name}
-        </button>
-        <p>
-          Edit <code>worker/index.ts</code> to change the name
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>UT Webring</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>
+              Name
+            </th>
+            <th>
+              Description
+            </th>
+            <th>
+              URL
+            </th>
+            <th>
+              Button
+            </th>
+          </tr> 
+        </thead>
+        <tbody>
+          {webring.map((site) => (
+            <tr>
+              <td>
+                {site.name}
+              </td>
+              <td>
+                {site.description}
+              </td>
+              <td>
+                <a href={site.link} target='_blank'>
+                  {site.url}
+                </a>
+              </td>
+              <td>
+                {site['88x31'] && <a href={site.link} target='_blank'><img src={site['88x31']} alt={`${site.name} 88x31 button`} /></a>}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   )
 }
