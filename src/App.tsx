d38f7f1d4dc3f9f0f60a06143a2837f7a49e963b
Razboy20 <razboy20@gmail.com>
2025-10-11 23:56:44 -0500

    chore: migrate to pnpm + biome
    (also lints, and other minor cleanups)

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,61 @@
-import { webring } from '../sites.json'
+import { webring } from "../sites.json";
 
 function App() {
-
   return (
     <>
       <h1>UT Webring</h1>
       <table>
         <thead>
           <tr>
-            <th>
-              Name
-            </th>
-            <th>
-              Description
-            </th>
-            <th>
-              URL
-            </th>
-            <th>
-              Button
-            </th>
-          </tr> 
+            <th>Name</th>
+            <th>Description</th>
+            <th>URL</th>
+            <th>Button</th>
+          </tr>
         </thead>
         <tbody>
           {webring.map((site) => (
-            <tr>
+            <tr key={site.name}>
+              <td>{site.name}</td>
+              <td>{site.description}</td>
               <td>
-                {site.name}
-              </td>
-              <td>
-                {site.description}
-              </td>
-              <td>
-                <a href={site.link} target='_blank'>
+                <a href={site.link} target="_blank">
                   {site.url}
                 </a>
               </td>
               <td>
-                {site['88x31'] && <a href={site.link} target='_blank'><img src={site['88x31']} alt={`${site.name} 88x31 button`} /></a>}
+                {site["88x31"] && (
+                  <a href={site.link} target="_blank">
+                    <img
+                      src={site["88x31"]}
+                      alt={`${site.name} 88x31 button`}
+                    />
+                  </a>
+                )}
               </td>
             </tr>
           ))}
         </tbody>
       </table>
       <div className="footer">
-        <a href="https://github.com/umbresp/ut-webring" target="_blank">GitHub Repository</a> | <a href="https://en.wikipedia.org/wiki/Webring" target="_blank">What's a webring?</a>
+        <a
+          href="https://github.com/umbresp/ut-webring"
+          target="_blank"
+          rel="noopener"
+        >
+          GitHub Repository
+        </a>{" "}
+        |{" "}
+        <a
+          href="https://en.wikipedia.org/wiki/Webring"
+          target="_blank"
+          rel="noopener"
+        >
+          What's a webring?
+        </a>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
